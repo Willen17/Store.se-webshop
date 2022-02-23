eb91f5b3846b3d793efa9943d5e4320f2d7a0a26
matsegard <matsegard@outlook.com>
2022-02-23 10:59:35 +0100

    Structured layout and added photos from unsplash api

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,16 +1,85 @@
-import React from "react";
-import "./Main.css";
+import { Component } from "react";
 import SectionCard from "./SectionCard";
+import "./Main.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-function Main() {
-  return (
-    <div className="main">
-      <SectionCard />
-      <SectionCard />
-      <SectionCard />
-      <SectionCard />
-    </div>
-  );
+interface ImageData {
+  id: string;
+  alt_description: string;
+  urls: {
+    full: string;
+    raw: string;
+    regular: string;
+    small: string;
+    small_s3: string;
+    thumb: string;
+  };
+}
+
+interface Props {}
+interface State {
+  imagesData: ImageData[];
+  likedImages: string[];
+}
+
+class Main extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { imagesData: [], likedImages: [] };
+  }
+
+  async componentDidMount() {
+    // Fetch images from API
+    const url = "https://api.unsplash.com/search/photos?page=1&query=landscape";
+    const response = await fetch(url, {
+      headers: {
+        Authorization: "Client-ID 5ZkcjZyK-7yptGcXOlS91s1s2a31zPil8AsqlKe-Zms",
+      },
+    });
+    const imagesData: ImageData[] = (await response.json()).results;
+
+    // Fetch liked images from LS
+    const likedImages = JSON.parse(localStorage.likedImages || "[]");
+
+    this.setState({ imagesData, likedImages });
+  }
+
+  toggleLikedImage(id: string) {
+    const { likedImages } = this.state;
+
+    // Remove/Copy
+    const newList = likedImages.filter((imageId) => imageId !== id);
+
+    // Add
+    if (newList.length === likedImages.length) {
+      newList.push(id); // It's ok to mutate newList since we made a copy above.
+    }
+
+    this.setState({ likedImages: newList });
+  }
+
+  componentDidUpdate() {
+    // Write to LS
+    localStorage.likedImages = JSON.stringify(this.state.likedImages);
+  }
+
+  render() {
+    return (
+      <main>
+        <div className="main-container">
+          {this.state.imagesData.map((imageData) => (
+            <SectionCard
+              isLiked={this.state.likedImages.includes(imageData.id)}
+              key={imageData.id}
+              src={imageData.urls.regular}
+              alt={imageData.alt_description}
+              onToggleLiked={() => this.toggleLikedImage(imageData.id)}
+            />
+          ))}
+        </div>
+      </main>
+    );
+  }
 }
 
 export default Main;
--- src/components/SectionCard.tsx
+++ src/components/SectionCard.tsx
@@ -1,25 +1,32 @@
-import { Component } from "react";
-import Main from "./Main";
+import { CSSProperties } from "react";
 import "./SectionCard.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartPlus } from "@fortawesome/free-solid-svg-icons";
+import { faCircleInfo } from "@fortawesome/free-solid-svg-icons";
 
-interface Props {}
-
-interface state {}
-
-class SectionCard extends Component {
-  constructor(props: Props) {
-    super(props);
-  }
-  render() {
-    return (
-      <div className="section-card">
-        <img
-          src="https://genesis-zone.com/media/cache/genesis_menu_product/Z23517_91075.png"
-          alt="headphones"
-        />
-        <p>product</p>
-      </div>
-    );
-  }
+interface Props {
+  isLiked: boolean;
+  src: string;
+  alt: string;
+  onToggleLiked: () => void;
 }
+
+function SectionCard(props: Props) {
+  return (
+    <div className="davidStyle">
+      <div className="frame">
+        <img className="img" src={props.src} alt={props.alt} />
+        {/* <i
+          onClick={props.onToggleLiked}
+          className={"icon " + (props.isLiked ? "liked" : "")}
+        ></i> */}
+        <div className="icons-container">
+          <FontAwesomeIcon className="info-icon" icon={faCircleInfo} />
+          <FontAwesomeIcon className="add-to-cart" icon={faCartPlus} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
 export default SectionCard;
