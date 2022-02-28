2f487f83250c5392fd0a17c2f402c56d7d0007a1
William Saar <williamsaar@outlook.com>
2022-02-28 16:16:57 +0100

    products now show up and can be added to an array

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,10 +1,10 @@
-import { Component } from "react";
+import { Component, createContext, useEffect, useState } from "react";
 import SectionCard from "./SectionCard";
 import "./Main.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useOutletContext } from "react-router-dom";
 
-interface ProductData {
+export interface ProductData {
   title: string;
   id: number;
   price: number;
@@ -15,86 +15,28 @@ interface ProductData {
 
 interface Props {}
 
-interface State {
-  productsData: ProductData[];
-}
+type ContextType = { products: ProductData[] | null };
 
-// api goes here
+function Main(props: Props) {
+  const [products, setProducts] = useState<ProductData[] | null>([]);
 
-class Main extends Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-  }
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products?limit=10")
+      .then((result) => result.json())
+      .then((products) => setProducts(products));
+  }, []);
 
-  async componentDidMount() {
-    const url = "https://fakestoreapi.com/products?limit=10"; // men's clothing
-    const response = await fetch(url);
-
-    const productsData = await response.json();
-
-    this.setState({ productsData });
-    console.log(this.state.productsData);
-  }
-
-  render() {
-    return (
-      <main>
-        <div className="main-container">
-          <Outlet />
-        </div>
-      </main>
-    );
-  }
+  return (
+    <main>
+      <div className="main-container">
+        <Outlet context={{ products }} />
+      </div>
+    </main>
+  );
 }
 
 export default Main;
 
-// randomPrice: number = Math.floor(Math.random() * (5000 - 500 + 1)) + 500;
-// async componentDidMount() {
-//   // Fetch images from API
-//   const url = "https://api.unsplash.com/search/photos?page=1&query=landscape";
-//   const response = await fetch(url, {
-//     headers: {
-//       Authorization: "Client-ID 5ZkcjZyK-7yptGcXOlS91s1s2a31zPil8AsqlKe-Zms",
-//     },
-//   });
-//   const imagesData: ImageData[] = (await response.json()).results;
-
-//   // Fetch liked images from LS
-//   // const shoppingCart = JSON.parse(localStorage.shoppingCart || "[]");
-//   const shoppingCart: ImageData[] = [];
-
-//   this.setState({ imagesData, shoppingCart: shoppingCart });
-// }
-
-// toggleLikedImage(id: string) {
-//   const { shoppingCart: shoppingCart } = this.state;
-
-//   // Remove/Copy
-//   const newList = shoppingCart.filter((imageId) => imageId !== id);
-
-//   // Add
-//   if (newList.length === shoppingCart.length) {
-//     newList.push(id); // It's ok to mutate newList since we made a copy above.
-//   }
-
-//   this.setState({ shoppingCart: newList });
-// }
-
-// addToCart(imageData: ImageData) {
-//   const { shoppingCart: shoppingCart } = this.state;
-
-//   // const copyOfCart = shoppingCart.filter((imageId) => imageId !== id);
-
-//   const copyOfCart = shoppingCart;
-
-//   copyOfCart.push(imageData);
-
-//   this.setState({ shoppingCart: copyOfCart });
-//   console.log(shoppingCart);
-// }
-
-// componentDidUpdate() {
-//   // Write to LS
-//   localStorage.shoppingCart = JSON.stringify(this.state.shoppingCart);
-// }
+export function useProducts() {
+  return useOutletContext<ContextType>();
+}
