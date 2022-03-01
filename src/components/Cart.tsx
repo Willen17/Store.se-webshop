f88a8eae60dd49471061e4ab528c912ded04d4c5
William Saar <williamsaar@outlook.com>
2022-03-01 16:33:38 +0100

    fixed cart styling and added total price amount

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,45 +1,38 @@
 import { faMinusSquare, faPlusSquare } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useEffect, useState } from "react";
-import { createEmitAndSemanticDiagnosticsBuilderProgram } from "typescript";
 import { ProductData } from "../interfaces/Interfaces";
 import "./Cart.css";
+import ErrorBoundary from "./ErrorBoundary";
 
 interface Props {
   cartItems: ProductData[];
-  handleAddProduct: (product: ProductData) => void;
+  onAddProduct: (product: ProductData) => void;
+  onRemoveProduct: (product: ProductData) => void;
 }
 
 function Cart(props: Props) {
-  const [totalCost, setTotalCost] = useState<number>(0);
-
-  useEffect(() => {
-    const costTotal = totalCost;
-    props.cartItems.map((cartitem) => setTotalCost(cartitem.price + costTotal));
-  }, [props.cartItems]);
-  // useEffect(() => {
-  //   props.cartItems.map((cartItem) => setTotalCost(totalCost + cartItem.price));
-  // }, [props.cartItems, totalCost]);
-
-  // props.cartItems.map((cartItem) => setTotalCost(totalCost + cartItem.price));
+  const totalCost = props.cartItems.reduce(
+    (sum, item) => sum + item.price * item.quantity,
+    0
+  );
 
   return (
     <div className="shopping-cart-main">
       <div className="shopping-cart-container">
         <div className="shopping-cart-header">
           <h1>Varukorg</h1>
-          <h1>Items</h1>
         </div>
         <div className="shopping-cart-item">
-          <ul className="product-info-title">
+          {/* <ul className="product-info-title">
             <li style={{ fontWeight: "bold" }}>Produkter</li>
-          </ul>
-          <ul className="product-info">
+          </ul> */}
+          {/* <ul className="product-info">
             <li style={{ fontWeight: "bold" }}>Antal</li>
             <li style={{ fontWeight: "bold" }}>Pris</li>
-            <li style={{ fontWeight: "bold" }}>Total</li>
-          </ul>
+          </ul> */}
         </div>
+
         {props.cartItems.map((cartItem) => (
           <div key={cartItem.id} className="shopping-cart-item">
             <img
@@ -59,17 +52,19 @@ function Cart(props: Props) {
               <FontAwesomeIcon
                 className="cart-icons"
                 icon={faPlusSquare}
-                onClick={() => props.handleAddProduct(cartItem)}
+                onClick={() => props.onAddProduct(cartItem)}
               />
               <FontAwesomeIcon
                 className="cart-icons"
                 icon={faMinusSquare}
-                // onClick={() => props.handleRemoveProduct(cartItem)}
+                onClick={() => props.onRemoveProduct(cartItem)}
               />
             </div>
           </div>
         ))}
-        <p>Total: {totalCost} $</p>
+        <div className="totalprice">
+          <p className="price">Total: {Math.round(totalCost)} $</p>
+        </div>
       </div>
     </div>
   );
