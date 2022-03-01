a6c164baf1aa93b1f053debba52afbeeda947049
William Saar <williamsaar@outlook.com>
2022-03-01 16:32:43 +0100

    moved product api fetch and state to products.tsx and fixed skeleton-loader for them

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,23 +1,43 @@
+import { useState, useEffect } from "react";
 import { ProductData } from "../interfaces/Interfaces";
 import ProductCard from "./ProductCard";
+import "./Products.css";
+import Skeleton from "./Skeleton";
 
 interface Props {
-  productItems: ProductData[];
-  handleAddProduct: (product: ProductData) => void;
+  onAddProduct: (product: ProductData) => void;
 }
 
 function Products(props: Props) {
-  return (
-    <div>
-      {props.productItems.map((product) => (
-        <ProductCard
-          handleAddProduct={props.handleAddProduct}
-          key={product.id}
-          productItem={product}
-        />
-      ))}
-    </div>
-  );
+  const [products, setProducts] = useState<ProductData[]>();
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products?limit=10")
+      .then((result) => result.json())
+      .then((products) => setProducts(products));
+  }, []);
+
+  if (products == null) {
+    // ["a", "b", "c", "d", "e"].map((id: string) => (
+    // return [1, 2, 3, 4, 5, 6, 7, 8, 9].map(() => <div className="skeleton" />);
+    return (
+      <div>
+        <Skeleton />
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        {products.map((product) => (
+          <ProductCard
+            handleAddProduct={props.onAddProduct}
+            key={product.id}
+            productItem={product}
+          />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default Products;
---
+++ src/components/Skeleton.tsx
@@ -0,0 +1,25 @@
+import "./Skeleton.css";
+
+function Skeleton() {
+  return (
+    <div>
+      <div className="davidStyle-skeleton">
+        <div className="product-container-skeleton">
+          <div className="img-skeleton">
+            <div className="loader" />
+          </div>
+        </div>
+      </div>
+
+      <div className="davidStyle-skeleton">
+        <div className="product-container-skeleton">
+          <div className="img-skeleton">
+            <div className="loader" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Skeleton;
